6258b7ba8f6b5208f5a69ede458a3e99cdf14519
aw <aw>
2026-06-06 17:43:48 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
+function setRemUnit() {
+  const html = document.documentElement
+  const width = html.clientWidth
+  const isLandscape = window.matchMedia('(orientation: landscape)').matches
+
+  if (width > 1000 || isLandscape) {
+    html.style.fontSize = '40px'
+  }
+  else {
+    html.style.fontSize = `${width / 10}px`
+  }
+}
+
+setRemUnit()
+window.addEventListener('resize', setRemUnit)
+
 createApp(App).mount('#app')
